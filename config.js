3410d2f7888b44998a25cdebc88ff6968ebed21a
Robby Schlesinger <robbyschlesinger@gmail.com>
2016-12-24 10:51:18 -0800

    make it better

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'zRBXTFr3IHbkHAnUCyx7mkcY6',
+  consumer_secret:      'Tyug7OBHOnZ6Fq4x4IZuT8VV6a8aRntRlsPgHvZqtezl1OHN3n',
+  access_token:         '3015436408-n9zcNM5CWIs7KoVWlCvvpBa3whaHFOESQLTkb0n',
+  access_token_secret:  '3bMh2wUZS37drRuIhkjvk9Mwdce5AKkoVHrisBNKo6Nsx'
+}
